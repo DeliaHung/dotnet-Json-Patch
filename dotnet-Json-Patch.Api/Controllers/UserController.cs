64a65daeef069a1662a3dfd04d66d26d131d3f4d
DeliaHung <pingchun.hung@gmail.com>
2023-07-21 00:17:24 +0800

    UPDATE

--- dotnet-Json-Patch.Api/Controllers/UserController.cs
+++ dotnet-Json-Patch.Api/Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using dotnet_Json_Patch.Api.Application.User.UpdateUserCommand;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_Json_Patch.Api.Controllers
@@ -13,28 +15,89 @@ namespace dotnet_Json_Patch.Api.Controllers
             _logger = logger;
         }
 
-        [http]
-
-        [HttpPost()]
-        public IActionResult AddUser(User user)
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetUserById(int id)
         {
-            return Ok();
+            User user = new()
+            {
+                Id = 1,
+                Email = "pingchun.hung@gmail.com",
+                Account = "delia",
+                Password = "123",
+                Birthday = new DateTime(1996, 03, 31),
+                FirstName = "hung",
+                LastName = "pung chun"
+            };
+            return Ok(user);
         }
 
-        [HttpPut("{id}")]
-        public IActionResult AddUser([FromRoute] Guid id, [FromBody] User user)
-        {
-            return Ok();
-        }
-        [HttpPatch()]
-        public IActionResult AddUser(User user)
-        {
-            return Ok();
-        }
+
         [HttpPost()]
-        public IActionResult AddUser(User user)
+        public async Task<IActionResult> CreateUser(User user)
         {
             return Ok();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateUserInfo(User user)
+        {
+            return Ok();
+        }
+
+        //[HttpPatch()]
+        //public async Task<IActionResult> CommonPatch(UpdateUserCommand command)
+        //{
+        //    User user = new()
+        //    {
+        //        Id = 1,
+        //        Email = "pingchun.hung@gmail.com",
+        //        Account = "delia",
+        //        Password = "123",
+        //        Birthday = new DateTime(1996, 03, 31),
+        //        FirstName = "hung",
+        //        LastName = "pung chun"
+        //    };
+
+        //    if (string.IsNullOrWhiteSpace(command.FirstName) == false)
+        //    {
+        //        user.FirstName = command.FirstName;
+        //    }
+
+        //    if (string.IsNullOrWhiteSpace(command.LastName) == false)
+        //    {
+        //        user.LastName = command.LastName;
+        //    }
+
+
+        //    return Ok(user);
+        //}
+
+        [HttpPatch("{id:guid}")]
+        public async Task<IActionResult> JsonPatch([FromBody] JsonPatchDocument<UpdateUserCommand> command)
+        {
+            User user = new()
+            {
+                Id = 1,
+                Email = "pingchun.hung@gmail.com",
+                Account = "delia",
+                Password = "123",
+                Birthday = new DateTime(1996, 03, 31),
+                FirstName = "hung",
+                LastName = "pung chun"
+            };
+
+            if (string.IsNullOrWhiteSpace(command.FirstName) == false)
+            {
+                user.FirstName = command.FirstName;
+            }
+
+            if (string.IsNullOrWhiteSpace(command.LastName) == false)
+            {
+                user.LastName = command.LastName;
+            }
+
+
+            return Ok(user);
         }
     }
 }
--- dotnet-Json-Patch.Api/User.cs
+++ dotnet-Json-Patch.Api/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string? Account { get; set; }
+        public string? Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? Birthday { get; set; }
     }
 }
